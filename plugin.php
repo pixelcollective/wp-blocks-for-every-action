b18508de7e5208fc94a1b5fab59eceeec4641f52
Kelly Mears <code@kellymears.me>
2018-11-18 18:46:44 -0800

    update to plugin.php header

--- plugin.php
+++ plugin.php
@@ -1,15 +1,14 @@
 <?php
 /**
- * Plugin Name: guten-every-action — CGB Gutenberg Block Plugin
- * Plugin URI: https://github.com/ahmadawais/create-guten-block/
- * Description: guten-every-action — is a Gutenberg plugin created via create-guten-block.
- * Author: mrahmadawais, maedahbatool
- * Author URI: https://AhmadAwais.com/
+ * Plugin Name: &nbsp;📋Every Action for Gutenberg
+ * Plugin URI: https://github.com/pixelcollective/every-action-for-gutenberg
+ * Description: Gutenberg support for NGP VAN Every Action.
+ * Author: Tiny Pixel Collective, Kelly Mears <developers@tinypixel.io>
+ * Author URI: https://tinypixel.io
  * Version: 1.0.0
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
  *
- * @package CGB
  */
 
 // Exit if accessed directly.
